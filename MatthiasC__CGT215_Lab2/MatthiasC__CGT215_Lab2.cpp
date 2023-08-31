fcdd8aa4d80eb369bbc93e726c030a4ed3468823
Curtis Lee Matthias <cmatthia@purdue.edu>
2023-08-31 10:40:55 -0400

    Lab 2

---
+++ MatthiasC__CGT215_Lab2/MatthiasC__CGT215_Lab2.cpp
@@ -0,0 +1,33 @@
+// MatthiasC__CGT215_Lab2.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    std::cout << "Hello World! My name is Curtis and I'm going to solve the equation A + B\n";
+   
+    float A;
+    float B;
+    float X;
+
+    cout << "Please enter a value for A: ";
+    cin >> A;
+
+    cout << "Please enter a value for B: ";
+    cin >> B;
+
+    cout << "A=" << A << endl;
+
+    cout << "And B=" << B << endl;
+
+    A + B ;
+    cout << "A + B=" << A + B << endl;
+
+
+
+
+
+}
